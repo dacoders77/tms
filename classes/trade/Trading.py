05841c30b298b4ebbcc70206d76144c4729e02bb
Pavel Perminov <pavel.perminov.23@gmail.com>
2020-12-10 23:10:20 +0300

    webhooks fix

--- classes/trade/Trading.py
+++ classes/trade/Trading.py
@@ -142,7 +142,7 @@ class TestApp(EWrapper, EClient):
                 urllib.request.urlopen(url).read()
 
         except:
-            error = 'Trading.py. Record req_id=' + self.nextValidOrderId + ' not found'
+            error = 'Trading.py. orderId ' + orderId + ' not found'
             print(error)
             self.log.error(error)
 
